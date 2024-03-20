f85b45d4a518bff47dbfc6ab1162a0e3898e3442
ikromjon0102 <ikromjon01021995@gmail.com>
2024-03-20 14:54:50 +0500

    list view va detail view

---
+++ news/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from .views import news_list_view, NewsListView, news_detail_view
+
+urlpatterns = [
+    # path('all/', news_list_view, name = 'news_list_page' ),
+    path('all/', NewsListView.as_view(), name = 'news_list_page' ),
+    path('<int:id>/', news_detail_view, name = 'news_detail_page' ),
+]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from news.models import News, Category
+
+
+def news_list_view(request):
+    all_news = News.objects.all()
+
+    context = {
+        'all_news': all_news,
+    }
+
+    return render(request, 'home.html', context=context)
+
+
+class NewsListView(ListView):
+    model = News
+    template_name = 'home.html'
+    context_object_name = 'all_news'
+
+
+
+def news_detail_view(request, id):
+    detail = News.objects.get(id = id)
+
+    context = {
+        'news_detail': detail,
+    }
+
+    return render(request, 'detail.html', context=context)
