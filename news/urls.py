c8a8ed5a677e2d0e3dd6328b035e339ad35ddae3
ikromjon0102 <ikromjon01021995@gmail.com>
2024-03-22 12:09:36 +0500

    category va single page

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import news_list_view, NewsListView, news_detail_view
+from .views import news_list_view, NewsListView, news_detail_view, sport_news_list_view
 
 urlpatterns = [
     path('all/', news_list_view, name = 'news_list_page' ),
+    path('sport/', sport_news_list_view, name = 'sport_news_page' ),
     # path('all/', NewsListView.as_view(), name = 'news_list_page' ),
     path('<int:id>/', news_detail_view, name = 'news_detail_page' ),
 ]
--- news/views.py
+++ news/views.py
@@ -6,16 +6,32 @@ from news.models import News, Category
 
 def news_list_view(request):
     all_news = News.published.all()
+    latest_post = News.published.all()[:5]
     sport_news_one = News.published.filter(category__name = 'Sport')[0]
     sport_news_all = News.published.filter(category__name = 'Sport')[1:5]
+    category = Category.objects.all()
 
     context = {
         'all_news': all_news,
         'sport_news_one': sport_news_one,
         'sport_news_all': sport_news_all,
+        'latest_post': latest_post,
+        'category': category
     }
-
     return render(request, 'index.html', context=context)
+
+def sport_news_list_view(request):
+    sport_news_one = News.published.filter(category__name = 'Sport')
+
+    context = {
+        'sport_news_all': sport_news_one,
+    }
+    return render(request, 'sport_page.html', context=context)
+
+
+
+
+
 
 
 class NewsListView(ListView):
@@ -32,7 +48,7 @@ def news_detail_view(request, id):
         'news_detail': detail,
     }
 
-    return render(request, 'detail.html', context=context)
+    return render(request, 'single_page.html', context=context)
 
 
 # def sport_news_list_view(request):
